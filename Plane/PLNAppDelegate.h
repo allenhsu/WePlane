2bdf40761ca72f3ee36ab8c2bfa55adbba74a166
Allen Hsu <me@imallen.com>
2013-09-09 21:00:57 +0800

    Initial Commit

---
+++ Plane/PLNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLNAppDelegate.h
+//  Plane
+//
+//  Created by Allen Hsu on 9/9/13.
+//  Copyright (c) 2013 Allen Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Plane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Plane
+//
+//  Created by Allen Hsu on 9/9/13.
+//  Copyright (c) 2013 Allen Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLNAppDelegate class]));
+    }
+}
